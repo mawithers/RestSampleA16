af7343660e397088e58bc5f4ded8dbf14f74a2b2
Mark.Withers <mark.withers@avalara.com>
2016-02-28 21:12:43 -0800

    Added and changed methods

--- RestSampleA16/Utility.cs
+++ RestSampleA16/Utility.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 using System.Configuration;
 using System.Net;
 using System.IO;
+using Newtonsoft.Json.Linq;
 
 
 namespace RestSampleA16  
@@ -15,38 +16,62 @@ namespace RestSampleA16
     {
         public static string PostJson(string url, string body)
         {
-            HttpWebRequest req = WebRequest.Create(new Uri(url))
-                                 as HttpWebRequest;
-            req.Method = "POST";
-            req.ContentType = "application/x-www-form-urlencoded";
-            req.ContentType = "application/json";
-            req.Headers.Add("Authorization", ConfigurationManager.AppSettings["Authorization"]);
-            req.Accept = "application/json; document-version=1";
-            req.UserAgent = ConfigurationManager.AppSettings["UserAgent"];
 
-
-            // Encode the parameters as form data:
-            byte[] formData = UTF8Encoding.UTF8.GetBytes(body);
-            req.ContentLength = formData.Length;
-
-            // Send the request:  You can use the "using" function on classes implementing IDispose interface. 
-            // Using will call Dispose method when that object goes out of scope and not wait on GC.
-            using (Stream post = req.GetRequestStream())
-            {
-                post.Write(formData, 0, formData.Length);
-            }
 
             // Pick up the response:
-            string result = null;
-            using (HttpWebResponse resp = req.GetResponse()
-                                          as HttpWebResponse)
+            HttpWebRequest req = WebRequest.Create(new Uri(url))
+                                  as HttpWebRequest;
+            
+            try
             {
-                StreamReader reader =
-                    new StreamReader(resp.GetResponseStream());
-                result = reader.ReadToEnd();
+
+
+                req.Method = "POST";
+                req.ContentType = "application/x-www-form-urlencoded";
+                req.ContentType = "application/json";
+                req.Headers.Add("Authorization", ConfigurationManager.AppSettings["Authorization"]);
+                req.Accept = "application/json; document-version=1";
+                req.UserAgent = ConfigurationManager.AppSettings["UserAgent"];
+
+
+                // Encode the parameters as form data:
+                byte[] formData = UTF8Encoding.UTF8.GetBytes(body);
+                req.ContentLength = formData.Length;
+
+                // Send the request:  You can use the "using" function on classes implementing IDispose interface. 
+                // Using will call Dispose method when that object goes out of scope and not wait on GC.
+                using (Stream post = req.GetRequestStream())
+                {
+                    post.Write(formData, 0, formData.Length);
+                }
+
+                // Pick up the response:
+                string result = null;
+                using (HttpWebResponse resp = req.GetResponse()
+                                              as HttpWebResponse)
+                {
+                    StreamReader reader =
+                        new StreamReader(resp.GetResponseStream());
+                    result = reader.ReadToEnd();
+                }
+
+                return result;
             }
 
-            return result;
+            catch (WebException wex)
+            {
+                if (wex.Response != null)
+                {
+                    using (var errorResponse = (HttpWebResponse)wex.Response)
+                    {
+                        using (var reader = new StreamReader(errorResponse.GetResponseStream()))
+                        {
+                            return reader.ReadToEnd().ToString();
+                        }
+                    }    
+                }
+                return wex.Message;
+            }
         }
 
         public static string GetValueBetween(string strSource, string strStart, string strEnd)
@@ -71,10 +96,10 @@ namespace RestSampleA16
                 return "";
             }
         }
-        public static void PutJsonInRavenDB(string json)
+        public static void PutJsonInRavenDB(string documentCode, string json)
         {
             string databaseName = "A16Documents";
-            int docId = 1;
+            string docId = documentCode;
             string entityName = "Documents";
             var url = string.Format(ConfigurationManager.AppSettings["RavenDBUrl"], databaseName, docId);
             var webRequest = System.Net.HttpWebRequest.CreateHttp(url);
@@ -88,6 +113,25 @@ namespace RestSampleA16
                 }
             var webResponse = webRequest.GetResponse();
             webResponse.Close();
+        }
+
+        public static string GetDocumentCode(string json)
+        {
+            JObject joResponse = JObject.Parse(json);
+  
+            JObject ojObject = (JObject)joResponse["header"];
+            
+            //JArray array = (JArray)ojObject["chats"];
+            
+            if (ojObject == null) 
+                {
+                    return "InvalidRequest";
+                }
+            else
+                {
+                    return ojObject.SelectToken("documentCode").ToString();
+                }  
+             
         }
     }
 }
